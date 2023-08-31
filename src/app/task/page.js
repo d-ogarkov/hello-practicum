32e22f88df425ec268cf18d59971f34629273f22
Dmitry Ogarkov <dima@ogarkov.com>
2023-08-31 21:42:51 +0300

    Исправление

--- src/app/task/page.js
+++ src/app/task/page.js
@@ -75,23 +75,23 @@ export default function Task() {
       <h1 className={styles.title}>Задание для подростков</h1>
       <p className={styles.text}>В&nbsp;программировании мы&nbsp;часто встречаемся со&nbsp;скобками: круглыми, фигурными, квадратными. В&nbsp;языке JavaScript в&nbsp;круглых скобках записываются аргументы функции или, например, условия. Фигурные скобки ограничивают тело функции. Квадратные скобки нужны для доступа к&nbsp;элементам массива или объекта.</p>
       <p className={styles.text}>Скобки в&nbsp;программе всегда парные. Если у&nbsp;нас есть открывающая скобка, то&nbsp;где-то должна быть и&nbsp;закрывающая. Такие пары скобок могут вкладываться друг в&nbsp;друга. Например, так:</p>
-      <Code children={code1}></Code>
+      <Code>{code1}</Code>
       <p className={styles.text}>Или так:</p>
-      <Code children={code2}></Code>
+      <Code>{code2}</Code>
       <p className={styles.text}>То, что находится между скобок, сейчас нас не&nbsp;интересует.</p>
       <p className={styles.text}>Если где-то не&nbsp;нашлось закрывающей или открывающей скобки, программа не&nbsp;будет работать. Вот здесь получим ошибку:</p>
-      <Code children={code3}></Code>
+      <Code>{code3}</Code>
       <p className={styles.text}>Мы&nbsp;напишем функцию на&nbsp;JavaScript, которая определяет, правильно&nbsp;ли написана условная &laquo;программа&raquo;, то&nbsp;есть все&nbsp;ли в&nbsp;порядке со&nbsp;скобками. Для простоты в&nbsp;&laquo;программе&raquo; будет одна строка и&nbsp;только круглые скобки. Вот две строки:</p>
-      <Code children={code4}></Code>
+      <Code>{code4}</Code>
       <p className={styles.text}>Напишем функцию isGood, которая берет строку str и&nbsp;в&nbsp;цикле for перебирает ее&nbsp;по&nbsp;одному символу:</p>
-      <Code children={code5}></Code>
+      <Code>{code5}</Code>
       <p className={styles.text}>Как нам определить, все&nbsp;ли хорошо со&nbsp;скобками в&nbsp;строке? Подумай, какой здесь может быть алгоритм. Что нам нужно подсчитать? И&nbsp;как в&nbsp;конце сделать вывод, есть в&nbsp;строке ошибки или нет?</p>
       <p className={styles.text}>Наша функция должна что-то делать (запоминать), когда встречает открывающую скобку: ( и&nbsp;когда закрывающую: ). Напишем эти условия:</p>
-      <Code children={code6}></Code>
+      <Code>{code6}</Code>
       <p className={styles.text}>Создадим переменную count, которую будем использовать в&nbsp;цикле для учета наших скобок. Пусть в&nbsp;начале она равна нулю:</p>
-      <Code children={code7}></Code>
+      <Code>{code7}</Code>
       <p className={styles.text}>Теперь твоя задача&nbsp;&mdash; написать эту функцию полностью. Если скобки в&nbsp;строке расставлены правильно, нужно вернуть результат true. А&nbsp;если есть ошибка&nbsp;&mdash; вернуть false:</p>
-      <Code children={code8}></Code>
+      <Code>{code8}</Code>
     </>
   );
 }
